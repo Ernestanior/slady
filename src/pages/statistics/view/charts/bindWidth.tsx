4c71f2cff7eb6be31d686db5974956b88aca1e8e
duke <duke_t@greypanel.com>
2022-01-27 18:14:19 +0800

    save code

--- src/pages/statistics/view/charts/bindWidth.tsx
+++ src/pages/statistics/view/charts/bindWidth.tsx
@@ -1,5 +1,9 @@
-import {FC} from "react";
+import {FC, useMemo} from "react";
 import {IDataModule} from "@/common/interface";
+import ReactECharts from "echarts-for-react";
+import * as echarts from "echarts";
+import moment from "moment";
+import {transformBindWidth, transformFlow} from "@/common/utils";
 
 export interface IBindWidth{
     bindWidth95: number;
@@ -7,8 +11,107 @@ export interface IBindWidth{
 }
 
 const BindWidth:FC<IDataModule<IBindWidth>> = ({data}) => {
+
+    const options = useMemo(() => {
+        if(!data || !data.bindWidthList){
+            return null
+        }
+         return {
+            backgroundColor: '#ffffff',
+            title: {
+                text: "带宽"
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter(params: any){
+                    let show = ''
+                    const fms = Array.isArray(params) ? params : [params];
+                    const date = moment(fms[0].data[0])
+                    const time = date.format("YYYY-MM-DD HH:mm");
+                    show += time + '<br>'
+                    const circleBgc = `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;background-color: #63b0ba;"></span>`
+                    fms.forEach(item => {
+                            // let marker = item as { marker: string }
+                            let value = item.value as [number, number]
+                            show += circleBgc + "带宽:" + transformBindWidth(value[1] ? value[1] : 0)
+                        }
+                    )
+                    return show;
+                }
+            },
+            toolbox: {
+                feature: {
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'time',
+                boundaryGap: false,
+                axisLabel: {
+                    rotate: 0,
+                },
+                splitNumber: 20,
+                splitLine: {
+                    show: true
+                },
+            },
+            yAxis: {
+                type: 'value',
+                name: "带宽",
+                nameTextStyle: {
+                    fontSize: 12,
+                    padding: [0, 0, 0, 95],
+                    color: "#666"
+                },
+                axisLabel: {
+                    formatter: (v: any) => {
+                        return transformFlow(v);
+                    },
+                },
+                axisTick: {
+                    show: false,
+                },
+            },
+            series: [
+                {
+                    type: 'line',
+                    showSymbol: false,
+                    symbol: 'none',
+                    animation: true,
+                    data: data.bindWidthList,
+                    lineStyle: {
+                        type: 'solid',
+                        color: "#63b0ba"
+                    },
+                    areaStyle: {},
+                    itemStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                offset: 0, color: "#ffffff"
+                            }, {
+                                offset: 1, color: "#63b0ba"
+                            }]
+                        )
+                    }
+                }
+            ]
+        }
+    }, [data])
+
+    if(!data || !data.bindWidthList){
+        return null
+    }
+
+    if(!options){
+        return null;
+    }
+
     return <section className="cdn-block">
-        带宽
+        <div>
+            <ReactECharts
+                option={options}
+            />
+        </div>
     </section>
 }
 
--- src/pages/statistics/view/charts/flow.tsx
+++ src/pages/statistics/view/charts/flow.tsx
@@ -1,5 +1,9 @@
-import {FC} from "react";
+import {FC, useMemo} from "react";
 import {IDataModule} from "@/common/interface";
+import ReactECharts from "echarts-for-react"
+import * as echarts from "echarts"
+import moment from "moment";
+import {transformFlow} from "@/common/utils";
 
 export interface IFlowData{
     flowList: any[] | null;
@@ -9,12 +13,102 @@ export interface IFlowData{
 
 const Flow:FC<IDataModule<IFlowData>> = ({data}) => {
 
-    if(!data){
+    const options = useMemo(() => {
+        if(!data || !data.flowList){
+            return null
+        }
+        const _:echarts.EChartsOption = {
+            backgroundColor: '#ffffff',
+            title: {
+                text: "流量"
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter(params: any){
+                    const fms = Array.isArray(params) ? params : [params];
+
+                    const date = moment(fms[0].data[0])
+
+                    return transformFlow(fms[0].data[1]) + "<br />" + date.format("YYYY-MM-DD HH:mm")
+                }
+            },
+            toolbox: {
+                feature: {
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'time',
+                boundaryGap: false,
+                axisLabel: {
+                    rotate: 0,
+                },
+                splitNumber: 20,
+                splitLine: {
+                    show: true
+                },
+            },
+            yAxis: {
+                type: 'value',
+                name: "流量",
+                nameTextStyle: {
+                    fontSize: 12,
+                    padding: [0, 0, 0, 95],
+                    color: "#666"
+                },
+                axisLabel: {
+                    formatter: (v: any) => {
+                        return transformFlow(v);
+                    },
+                },
+                axisTick: {
+                    show: false,
+                },
+            },
+            series: [
+                {
+                    type: 'line',
+                    symbol: 'none',
+                    sampling: 'lttb',
+                    itemStyle: {
+                        color: "#BD2719"
+                    },
+                    showSymbol: false,
+                    animation: true,
+                    areaStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                            {
+                                offset: 0,
+                                color: 'rgb(255, 158, 68)'
+                            },
+                            {
+                                offset: 1,
+                                color: 'rgb(255, 70, 131)'
+                            }
+                        ])
+                    },
+                    data: data.flowList
+                }
+            ]
+        }
+        return  _
+    }, [data])
+
+    if(!data || !data.flowList){
         return null
     }
 
-    return <section className="cdn-block">
+    if(!options){
+        return null;
+    }
 
+    return <section className="cdn-block">
+        <div>
+            <ReactECharts
+                option={options}
+            />
+        </div>
     </section>
 }
 
