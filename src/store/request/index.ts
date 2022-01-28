234aba349f1e069df5cc0c0de006caf1df123085
duke <duke_t@greypanel.com>
2022-01-28 09:29:38 +0800

    操作成功更新提示

--- src/store/request/index.ts
+++ src/store/request/index.ts
@@ -2,6 +2,7 @@ import RequestPlx from "@/common/tools/request";
 import {getToken} from "@/store/request/token";
 import {AxiosRequestConfig} from "axios";
 import {notification} from "antd";
+import accountService from "@/store/account/service";
 
 const requestPlx = new RequestPlx();
 
@@ -38,6 +39,9 @@ requestPlx.middleware_after.use(async (rep, next) => {
             result: rep.statusText,
             message: rep.statusText
         };
+        if(rep.status){
+            accountService.autoLogout();
+        }
     }
     await next()
 })
@@ -69,6 +73,20 @@ requestPlx.middleware_after.use(async (rep, next) => {
 async function request<T>(config: AxiosRequestConfig){
     const rep = await requestPlx.request(config);
     if(rep.data){
+        if(rep.data.isSuccess){
+            if(config.method && config.method.toUpperCase() === "PUT"){
+                // 创建操作
+                if(config.url && config.url.toLowerCase().indexOf("create") > -1){
+                    notification.success({
+                        message: "创建成功"
+                    })
+                }else{
+                    notification.success({
+                        message: "配置已更新"
+                    })
+                }
+            }
+        }
         return rep.data as IRequestResult<T>;
     }else{
         return {
