943c9ad33bf427a8d98f7e82a42bc364279ba99c
tangerwei <tangerwei@foxmail.com>
2022-03-30 09:54:42 +0800

    修改站点搜索的字段

--- src/pages/cdn/site/filters.tsx
+++ src/pages/cdn/site/filters.tsx
@@ -7,10 +7,10 @@ import {CONST_SITE_TYPE} from "@/pages/cdn/site/index";
 
 const CDNSiteListFilter:FC = () => {
     return <>
-        <FormItem span={3} noStyle name="name">
+        <FormItem span={3} noStyle name="siteName">
             <Input placeholder="站点名称" allowClear />
         </FormItem>
-        <FormItem span={3} noStyle name="displayName">
+        <FormItem span={3} noStyle name="type">
             <SelectP emptyOption data={CONST_SITE_TYPE} placeholder="站点类型" />
         </FormItem>
         <FormItem span={4} noStyle name="customerId">
