f22af2594d636568f0740ddb351e492a6e5dd71a
ernest_l <ernest_l@greypanel.com>
2022-07-15 15:19:37 +0800

    邮件页面解决单张图的情况下检测不到内容bug

--- src/pages/news/editor.tsx
+++ src/pages/news/editor.tsx
@@ -22,18 +22,17 @@ function MyEditor(props: IProps) {
     const imgUrlCallbackRef = useUpdateRef(props.imgUrlCallback)
     const value$ = useRef(new Subject<string | undefined>())
     const [html, setHtml] = useState("")
-
     useEffect(() => {
         value$.current.next(props.value);
     }, [value$, props.value])
 
     useEffect(() => {
         const sub = value$.current.pipe(debounceTime(500)).subscribe(value => {
-            console.log(value)
             if(!!value){
                 const valueHtml = value.indexOf("<p>") !== 0
                     ? value.split(/\n/).map(line => `<p>${line}</p>`).join('\n') : value
                 setHtml(valueHtml)
+                console.log(valueHtml)
             }
         })
         return () => sub.unsubscribe()
@@ -77,9 +76,12 @@ function MyEditor(props: IProps) {
 
     const changeHandler = useCallback((editor: IDomEditor) => {
         // 未开启初始化完成标签，禁止onChange事件
-        if(!editor.getText()){
+        if(editor.isEmpty()){
             return
         }
+        // if(!editor.getText()){
+        //     return
+        // }
         onChangeRef.current && onChangeRef.current(editor.getHtml())
     }, [onChangeRef])
 
