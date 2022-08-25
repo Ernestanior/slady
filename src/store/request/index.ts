828a0092b460ac208f003676b8d1d13bb1afb977
ernest_l <ernest_l@greypanel.com>
2022-08-25 14:35:12 +0800

    新增客服切换功能

--- src/store/request/index.ts
+++ src/store/request/index.ts
@@ -6,8 +6,8 @@ import accountService from "@/store/account/service";
 
 const requestPlx = new RequestPlx();
 
-// const dev_url = 'http://localhost:10087';
-const dev_url = 'http://192.168.8.5:10087';
+const dev_url = 'http://localhost:10087';
+// const dev_url = 'http://192.168.8.15:10087';
 
 // add dev server url
 requestPlx.middleware_before.use(async (config, next) => {
